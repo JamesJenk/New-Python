8a086e50abf84fcac4f266919ed9a6b5c54f046f
JenkJ <james_a_jenkins@outlook.com>
2023-02-01 13:00:05 -0500

    hi

--- instagram.py
+++ instagram.py
@@ -6,6 +6,7 @@ import pyautogui
 from subprocess import Popen
 
 print('Inputs in chronological order: hour to send, minute to send, username, and message, any mistakes you will have to cancel and restart programme.')
+print('For programme to execute at midnight, minute and hour should be set to 0')
 
 # Getting time and contents of message 
 
@@ -22,68 +23,146 @@ username = input()
 print('What is your message?')
 messages = input()
 
+# If it is for midnight then the message will send when the day changes
+
+if hours == 0:
+        date = dt.datetime.now()
+        day = date.strftime('%d')
+
+# print date that the command has been issued
+
+issue = dt.datetime.now().strftime('%Y:%m:%d:%H:%M:%S')
+
+sleep(1)
+
+print('Command issued @', issue)
+
 # creating a datetime filled with crap minus the hour and minute which will be stripped allowing datetime to be happy and we take what we want
 
 desired_time = dt.datetime(year=2021, month=2, day=2, hour=hours, minute=minutes, second=2)
 desired_time = desired_time.strftime('%H:%M')
 
+print('Desired time is: ', desired_time)
+
 # for loop checking time, then if statement executes command
+
+# creating print check requirement (pcr)
+
+pcr = dt.datetime(year=2021, month=2, day=2, hour=hours, minute=30, second=2)
+pcr = pcr.strftime('%M')
 
 while True:
     time = dt.datetime.now()
     now = time.strftime("%H:%M")
+    days = time.strftime('%d')
+    print_check = time.strftime('%M')
 
-    if now >= desired_time:
+    current = time.strftime('%Y:%m:%d:%H:%M:%S')
 
-# prints time
+# try not to burn cpu so ping every 30 seconds
 
-                Popen('firefox https://www.instagram.com/direct/inbox/', shell=True)
+    sleep(30)
 
-                sleep(5)
+# print specs every 30 mins
 
-# goes to the search for person tab
+    if print_check == pcr:
+        print('Current time is: ', current, 'Time specefied is: ', desired_time)
 
-                for i in range(0,12):
-                        sleep(0.5)
+
+# If not midnight then proceed normally, if midnight then will execute
+
+    if hours != 0:
+                if now >= desired_time:
+
+        # prints time
+
+                        Popen('firefox https://www.instagram.com/direct/inbox/', shell=True)
+
+                        sleep(5)
+
+        # goes to the search for person tab
+
+                        for i in range(0,12):
+                                sleep(0.5)
+                                pyautogui.press('tab')
+
+        # enters and selects name
+
+                        sleep(1)
+                        pyautogui.press('enter')
+                        sleep(1)
+                        pyautogui.write(username, interval=0.25)
+                        sleep(1)
                         pyautogui.press('tab')
+                        sleep(1)
+                        pyautogui.press('enter')
+                        sleep(1)
 
-# enters and selects name
+        # skips through other options and finishes
 
-                sleep(1)
-                pyautogui.press('enter')
-                sleep(1)
-                pyautogui.write(username, interval=0.25)
-                sleep(1)
-                pyautogui.press('tab')
-                sleep(1)
-                pyautogui.press('enter')
-                sleep(1)
+                        for i in range(0,25):
+                                sleep(0.3)
+                                pyautogui.press('tab')
 
-# skips through other options and finishes
 
-                for i in range(0,25):
-                        sleep(0.3)
+                        pyautogui.press('enter')
+
+                        sleep(2)
+
+                        # message is written and then sent
+
+                        pyautogui.write(messages, interval=0.25)
+
+                        pyautogui.press('enter')
+
+
+                        
+                        break
+
+    else:
+
+        if day != days:
+
+                        Popen('firefox https://www.instagram.com/direct/inbox/', shell=True)
+
+                        sleep(5)
+
+        # goes to the search for person tab
+
+                        for i in range(0,12):
+                                sleep(0.5)
+                                pyautogui.press('tab')
+
+        # enters and selects name
+
+                        sleep(1)
+                        pyautogui.press('enter')
+                        sleep(1)
+                        pyautogui.write(username, interval=0.25)
+                        sleep(1)
                         pyautogui.press('tab')
+                        sleep(1)
+                        pyautogui.press('enter')
+                        sleep(1)
+
+        # skips through other options and finishes
+
+                        for i in range(0,25):
+                                sleep(0.3)
+                                pyautogui.press('tab')
 
 
-                pyautogui.press('enter')
+                        pyautogui.press('enter')
 
-                sleep(2)
+                        sleep(2)
 
-                # message is written and then sent
+                        # message is written and then sent
 
-                pyautogui.write(messages, interval=0.25)
+                        pyautogui.write(messages, interval=0.25)
 
-                pyautogui.press('enter')
+                        pyautogui.press('enter')
 
-
-                
-                break
-
-
-sleep(1)
-
-pyautogui.hotkey('alt', 'tab')
+                        break
 
 sleep(1)
 
